1e0c689f370b31a31970686c2b5a3a96e219ea42
C NASIR <cnasir22.0@gmail.com>
2021-10-07 11:46:46 -0500

    Initial first commit

---
+++ Lab4II/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Lab4II
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
