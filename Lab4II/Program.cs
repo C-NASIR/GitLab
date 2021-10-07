fec91007729df3ed3eba2d2b6317171b2c07d46d
stephamundson <stephamundson@gmail.com>
2021-10-07 17:13:23 -0500

    Added the comment block and signature of a new method "IsDiagonal()"

--- Lab4II/Program.cs
+++ Lab4II/Program.cs
@@ -8,5 +8,16 @@ namespace Lab4II
         {
             Console.WriteLine("Hello World!");
         }
+
+        /// <summary>
+        ///     Checks if the 2D array is both a square and contains only diagonal elements
+        ///     (where non-diagonal elements are 0).
+        /// </summary>
+        /// <param name="data">the 2D array passed in to be checked</param>
+        /// <returns>true if the array is a square and contains only diagonal elements. Otherwise, false.</returns>
+        public Boolean IsDiagonal(int[][] data)
+        {
+
+        }
     }
 }
