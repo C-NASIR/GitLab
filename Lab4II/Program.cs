8940a3f14356a1bac87390ee2a0d6a94d8fae983
C NASIR <cnasir22.0@gmail.com>
2021-10-11 21:46:15 -0500

    implemented the IsDiagonal method

--- Lab4II/Program.cs
+++ Lab4II/Program.cs
@@ -15,9 +15,17 @@ namespace Lab4II
         /// </summary>
         /// <param name="data">the 2D array passed in to be checked</param>
         /// <returns>true if the array is a square and contains only diagonal elements. Otherwise, false.</returns>
-        public Boolean IsDiagonal(int[][] data)
+        public Boolean IsDiagonal(int[,] data)
         {
-
+            if (data.GetLength(0) == data.GetLength(1))
+            {
+                if (data[0, 0] == 0) return false;
+                if (data[0, data.GetLength(1) - 1] == 0) return false;
+                if (data[data.GetLength(0) - 1, 0] == 0) return false;
+                if (data[data.GetLength(0) - 1, data.GetLength(1) - 1] == 0) return false;
+                return true;
+            }
+            return false;
         }
     }
 }
